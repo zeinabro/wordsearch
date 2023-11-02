e1d033cce38f66de2f6c88199cf01ada429bd1c7
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-11-02 21:55:48 +0000

    changed col row classes to data attributes#

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 async function generateBoard() {
-    await getWords()
+    const words = await getWords()
+    console.log(words)
     board.innerHTML = ''
 
     const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
@@ -8,23 +9,34 @@ async function generateBoard() {
     let numRow = 0
     let numCol = 0
 
+    words.forEach((word) => {
+
+    })
+
     for (let i=0; i<60; i++){
         const rand = alphabet[Math.floor(Math.random()*26)]
         let letter = document.createElement('div')
         letter.className = `letter ${i}`
         if (letter.textContent=='') {letter.textContent = rand}
         letter.style.padding = letter.textContent==''?'9px':'0px'
+        letter.addEventListener('click', () => {
+            letter.style.backgroundColor = 'grey'
+        })
         board.appendChild(letter)
 
         //matrix for 6 cols 10 rows
         if (i == 0 || i % 6 == 0) {
             row = [rand]
-            letter.classList.add('row0', `col${numCol}`)
+            // letter.classList.add(`row${numRow}`, `col${numCol}`)
+            letter.dataset.row = numRow
+            letter.dataset.column = numCol
             numCol++
         }
         else if (i % 6 > 0) {
             row.push(rand)
-            letter.classList.add(`row${numRow}`, `col${numCol}`)
+            // letter.classList.add(`row${numRow}`, `col${numCol}`)
+            letter.dataset.row = numRow
+            letter.dataset.column = numCol
             numCol++
         }
         
@@ -40,20 +52,35 @@ async function generateBoard() {
 
 async function getWords() {
     //https://api.datamuse.com/words
+
     words_list.innerHTML = ''
+
     let topic = 'intelligent'
     let max = 5
+
     const resp =  await fetch(`https://api.datamuse.com/words?ml=${topic}&max=${max+5}`)
-    const words = await resp.json()
-    console.log(words)
+    const data = await resp.json()
+
     let words_length = 0
-    for (let i=0; i<max; i++){
-        const word = document.createElement('li')
-        word.className = `word ${i}`
-        word.textContent = words[i].word
-        words_list.appendChild(word)
-        words_length += (words[i].word.length)
+    let num_words = 0
+    let i = 0
+    let words = []
+
+    while (num_words < 5){
+        const word = data[i].word
+        if (!word.includes(' ') && !word.includes('-') && word.length<10){
+            const word_item = document.createElement('li')
+            word_item.className = `word ${i}`
+            word_item.textContent = word
+            words_list.appendChild(word_item)
+            words_length += (word.length)
+            num_words++ 
+            words.push(word)           
+        }
+        i++
     }
+    console.log(words_length)
+    return words
 }
 
 const board = document.getElementById('board')
