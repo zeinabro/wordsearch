cbe902c8e0776472d58e440698b8d06613602635
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-11-05 14:26:03 +0000

    placing words

--- assets/index.js
+++ assets/index.js
@@ -47,51 +47,65 @@ async function place_words() {
             options.push('horizontal')
             options.push('diagonal') 
         }
-        let option = options[Math.floor(Math.random()*3)] 
-
+        // let option = options[Math.floor(Math.random()*3)] 
         let available = false
+        let counter=0
         while (available==false){
+            let option = options[Math.floor(Math.random()*3)] 
             let start_pos = find_start_pos(word, option)
             let empty = check_placement(word,option,start_pos)
+            console.log(word, option,"empty: ",empty)
 
-            let start_tile = document.querySelector(`[data-row="${start_pos[0]}"][data-column="${start_pos[1]}"]`)
-
-            if (empty){
-                // start_tile.textContent = word[0]
-                matrix[start_pos[0]][start_pos[1]] = word[0]
-                start_tile.style.backgroundColor = 'pink'
+            if (empty==true){
                 available=true
-            }  
+            } else if (counter>10){
+                let index = words.indexOf(word)
+                let word_el = document.getElementsByClassName(`word ${index}`)
+                console.log(word_el)
+                word_el.innerHTML= ""
+            } else if (empty==false){
+                counter++
+            }
         }
     })
     place_random_letters()
 }
 
 function find_start_pos(word, option){
+    // console.log(matrix)
     let start_pos
-    if (option=='horizontal'){
-        start_pos = [Math.floor(Math.random()*numRows), Math.floor(Math.random()*(numCols+1-word.length))]
-    } else if (option=='vertical'){
-        start_pos = [Math.floor(Math.random()*(numRows+1-word.length)), (Math.floor(Math.random()*numCols))]
-    } else {
-        let len = word.length
-        let combos = []
-        if (len<numCols) {
-            combos.push([(numCols-2)+(numCols-len),0])
-            for (let c=1;c<numCols-len;c++){
-                if (combos.indexOf([4+numCols-len,c])==-1){
-                    combos.push([4+numCols-len,c])  
-                } 
+    let empty = false
+    while (empty==false){
+        if (option=='horizontal'){
+            start_pos = [Math.floor(Math.random()*numRows), Math.floor(Math.random()*(numCols+1-word.length))]
+        } else if (option=='vertical'){
+            start_pos = [Math.floor(Math.random()*(numRows+1-word.length)), (Math.floor(Math.random()*numCols))]
+        } else {
+            let len = word.length
+            let combos = []
+            if (len<numCols) {
+                combos.push([(numCols-2)+(numCols-len),0])
+                for (let c=1;c<numCols-len;c++){
+                    if (combos.indexOf([4+numCols-len,c])==-1){
+                        combos.push([4+numCols-len,c])  
+                    } 
+                }
             }
+            for (let c=0;c<(numCols-len)+1;c++){
+                for (let r=0;r<(4+(numCols-len)+1);r++){
+                    if (combos.indexOf([r,c]) == -1){combos.push([r,c])}
+                }
+            } 
+            start_pos=combos[Math.floor(Math.random()*(combos.length-1))]
         }
-        for (let c=0;c<(numCols-len)+1;c++){
-            for (let r=0;r<(4+(numCols-len)+1);r++){
-                if (combos.indexOf([r,c]) == -1){combos.push([r,c])}
-            }
-        } 
-        start_pos=combos[Math.floor(Math.random()*(combos.length-1))]
+
+        let start_tile = document.querySelector(`[data-row="${start_pos[0]}"][data-column="${start_pos[1]}"]`)
+
+        if (start_tile.textContent == ""){
+            empty=true
+        }
+        // empty=true
     }
-    
     return start_pos
 }
 
@@ -100,45 +114,85 @@ function check_placement(word,option,start_pos){
     let start_tile = document.querySelector(`[data-row="${start_pos[0]}"][data-column="${start_pos[1]}"]`)
 
     let empty = true //tile is empty
-    if (start_tile.textContent == ""){
-        //[row,col] = [y,x]
+    // if (start_tile.textContent == ""){
+        //[row,col] -> [y,x]
+        let x = start_pos[1]
+        let y = start_pos[0]
         if (option == "horizontal"){
-            console.log(start_tile)
-            console.log(word,option,start_pos)
-
-            let x = start_pos[1]
-            let y = start_pos[0]
-
-            while (empty==true && x<numCols-1 && x<numCols-start_pos[1]+1){
+            while (empty==true && x<numCols-1 && x<start_pos[1]+word.length-1){
                 x++
                 let next_tile = document.querySelector(`[data-row="${y}"][data-column="${x}"]`)
                 empty = (next_tile.textContent=="") ? true : false
-                console.log(next_tile,next_tile.textContent,empty)
             }
-            //if available ?
             if (empty){
-                console.log('empty')
                 let i = 0
-                console.log(word,word.length)
                 for (let x=start_pos[1];x<word.length+start_pos[1];x++){
-                    console.log(x)
                     let next = document.querySelector(`[data-row="${y}"][data-column="${x}"]`)
                     next.style.backgroundColor = 'yellow'
                     next.textContent = word[i]
-                    console.log(next,word[i])
                     matrix[y][x] = word[i]
                     i++
                 }
             }
         }
         else if (option == 'vertical'){
-
+            //same col, row++ ->
+            console.log(word,'vertical')
+            while (empty==true && y<numRows-1 && y<start_pos[0]+word.length-1){
+                y++
+                console.log(y)
+                let next_tile = document.querySelector(`[data-row="${y}"][data-column="${x}"]`)
+                console.log([y,x],next_tile)
+                console.log("is empty: ",next_tile.textContent == "")
+                empty = (next_tile.textContent=="") ? true : false
+                console.log(empty)
+            }
+            if (empty){
+                let i = 0
+                for (let y=start_pos[0];y<word.length+start_pos[0];y++){
+                    let next = document.querySelector(`[data-row="${y}"][data-column="${x}"]`)
+                    next.style.backgroundColor = 'pink'
+                    next.textContent = word[i]
+                    matrix[y][x] = word[i]
+                    i++
+                }
+            }
         }
-        // return empty
-    } else {
-        empty = false
-    }
-    return true
+        else if (option == 'diagonal'){
+            //[row,col] -> [y,x]
+            console.log(word,word.length,y,x)
+
+            while (empty==true && x<numCols-1 && y<numRows-1 && x<start_pos[1]+word.length-1 && y<start_pos[0]+word.length-1){
+
+            // while (empty==true && x<numCols-1 &&x<numCols-start_pos[1]+1 && y<numRows-1 && y<word.length-start_pos[0]+1){
+                y++
+                x++
+                console.log("d ",y,x)
+                let next_tile = document.querySelector(`[data-row="${y}"][data-column="${x}"]`)
+                empty = (next_tile.textContent=="") ? true : false
+                if (empty==false){
+                    console.log('not empty',next_tile.textContent)
+                }
+                console.log(next_tile,empty)
+            }
+            if (empty){
+                let i = 0
+                let x = start_pos[1]
+                for (let y=start_pos[0];y<word.length+start_pos[0];y++) {
+                    let next = document.querySelector(`[data-row="${y}"][data-column="${x}"]`)
+                    next.style.backgroundColor='green'
+                    next.textContent = word[i]
+                    matrix[y][x] = word[i]
+                    i++
+                    x++
+                }
+            }
+        }
+    // } else {
+    //     console.log('else')
+    //     empty = false
+    // }
+    return empty
 }
 
 function place_random_letters() {
